910879b5cb1e7acd2db320b81382157a281de11a
Hannah Sheldon <hannahzsheldon@gmail.com>
2024-07-16 16:14:33 -0700

    end of Hannah's turn

--- aa03-recursion-pt1/problems/02-sum-array.js
+++ aa03-recursion-pt1/problems/02-sum-array.js
@@ -11,9 +11,22 @@ sumArray([1, 2, 3, 4, 5]); // 15
 ***********************************************************************/
 
 function sumArray(arr) {
-  // Your code here 
+  // let sum = 0;
+  if (arr.length > 0) {
+    let [var1, ...restArr] = arr;
+    return var1 + sumArray(restArr);
+  } else return 0;
 }
-  
+
+/*
+function tester(...restParameters) {}; //restParameter => [p1, p2, p3];
+tester(p1, p2, p3)
+
+let arr = [1, 2, 3, 4];
+[p1, p2, p3, p4] = arr;
+[p1, ...restOfArray] = arr; // restOfArray => [2, 3, 4];
+*/
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumArray;
